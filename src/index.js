48335007fff99fe5ff1483a5fa8ca9c260fbc253
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-07-11 23:35:28 +0530

    basic structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 console.log('hello mf');
+var content = document.querySelector('#content');
+var body = document.querySelector('body')
+var buttonDiv = document.createElement('div')
+function deletegrid(container)
+{
+    var cells = container.lastElementChild;
+    while(cells)
+    {
+        container.removeChild(cells);
+        cells = container.lastElementChild;
+    }
+}
+
+const home = ()=> {
+    var home_content = document.createElement('div');
+    
+    var home_text = document.createElement('h1');
+    home_text.textContent = 'restraunt is good';
+
+    home_content.appendChild(home_text);
+    content.appendChild(home_content);
+}
+
+
+const home_btn = document.createElement('button');
+home_btn.innerHTML = 'Home';
+home_btn.addEventListener('click', ()=> {
+    deletegrid(content);
+    home();
+})
+
+const menu_btn = document.createElement('button');
+menu_btn.innerHTML = 'Menu';
+
+const contact_btn = document.createElement('button');
+contact_btn.innerHTML = 'Contact';
+
+buttonDiv.append(home_btn, menu_btn, contact_btn);
+body.appendChild(buttonDiv);
+
+home();
---
+++ tasks.md
@@ -0,0 +1,3 @@
+cretae all stuff in a single div 
+
+and append it every time a button is clicked 
