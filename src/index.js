3b76350f1750fb79ee926fa2fc4543c7b9a37a41
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-07-13 10:45:39 +0530

    made it functional

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 console.log('hello mf');
 var content = document.querySelector('#content');
 var body = document.querySelector('body')
-var buttonDiv = document.createElement('div')
+
+//to clear the content div every time
 function deletegrid(container)
 {
     var cells = container.lastElementChild;
@@ -22,21 +23,52 @@ const home = ()=> {
     content.appendChild(home_content);
 }
 
+const menu = () => {
+    var menu_content = document.createElement('div');
+
+    var menu_text = document.createElement('h1');
+    menu_text.textContent = 'All Menu Stuff';
+
+    menu_content.appendChild(menu_text);
+    content.appendChild(menu_content)
+}
+
+const contact = () => {
+    var contact_content = document.createElement('div');
+
+    var contact_text = document.createElement('h1');
+    contact_text.textContent = 'Contact us here';
+
+    contact_content.appendChild(contact_text);
+    content.appendChild(contact_content);
+}
+
 
 const home_btn = document.createElement('button');
 home_btn.innerHTML = 'Home';
 home_btn.addEventListener('click', ()=> {
     deletegrid(content);
+    buttonbar();
     home();
 })
 
 const menu_btn = document.createElement('button');
 menu_btn.innerHTML = 'Menu';
+menu_btn.addEventListener('click', ()=> {
+    deletegrid(content);
+    buttonbar()
+    menu();
+})
 
 const contact_btn = document.createElement('button');
 contact_btn.innerHTML = 'Contact';
-
-buttonDiv.append(home_btn, menu_btn, contact_btn);
-body.appendChild(buttonDiv);
-
+contact_btn.addEventListener('click', ()=> {
+    deletegrid(content);
+    buttonbar()
+    contact();
+})
+const buttonbar = ()=> {
+    content.append(home_btn, menu_btn, contact_btn);
+}
+buttonbar();
 home();
